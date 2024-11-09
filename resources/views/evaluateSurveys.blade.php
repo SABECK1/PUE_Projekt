c95acfaee2f5c860d13e4978cba18ac28a7c31ce
DoescherJonas <jonasdoe2003@gmail.com>
2024-11-09 15:30:24 +0100

    Basic site construction

--- resources/views/evaluateSurveys.blade.php
+++ resources/views/evaluateSurveys.blade.php
@@ -1,4 +1,43 @@
 @extends('layout')
 
 @section('content')
+
+    @php
+        // Bestimme das Icon basierend auf dem Wert von "Vorjahreswert"
+        $icon = 'm-arrow-right'; // Standardwert bei Gleichheit
+
+        if (2 > 1)
+        {
+            $icon = 'o-arrow-trending-up';
+        } elseif (1 < 1)
+        {
+            $icon = 'o-arrow-trending-down';
+        }
+    @endphp
+    <x-header title="Umfrage auswerten" separator/>
+
+    <div class="flex justify-around mt-4 space-x-4">
+        <x-stat title="Auswertungen letztes Jahr" value="10" icon="m-swatch"/>
+        <x-stat title="Durchschnittswert" description="Letztes Jahr" value="3" icon="m-tv"/>
+        <x-stat title="Auswertungen  dieses Jahr" value="44" icon="m-swatch"/>
+        <x-stat title="Durchschnittswert" description="Dieses Jahr" value="4" icon="{{ $icon }}"  />
+    </div>
+
+    @foreach($surveys as $survey)
+    <x-list-item :item="$survey">
+        <x-slot:value>
+            {{ $survey['class'] }}
+        </x-slot:value>
+        <x-slot:sub-value>
+            {{ $survey['date'] }}
+        </x-slot:sub-value>
+        <x-slot:actions>
+            <x-button icon="c-arrow-turn-right-up" class="text-red-500"
+                      link="{{ route('evaluateSurveyData', [
+                              'survey' => urlencode(json_encode($survey)) // Serialize and URL encode the survey
+                          ]) }}" />
+        </x-slot:actions>
+    </x-list-item>
+    @endforeach
+
 @endsection
