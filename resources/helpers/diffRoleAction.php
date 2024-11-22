788265a73847e001f6de7e704c3471821be1184b
DieBratkartoffel <juri.gontard@wieder.de>
2024-11-22 14:27:11 +0100

    Anzeige der survey data überarbeitet und funktion hinzugefügt

--- resources/helpers/diffRoleAction.php
+++ resources/helpers/diffRoleAction.php
@@ -13,3 +13,20 @@ function getLinkByUserRole($userRole) {
             return null;
     }
 }
+
+function calculateBewertungColour($value)//Übergabe der zur berechnung benötigten parameter. evnt. Survey mit den fragen und dazugehörigen antworten
+{         
+    
+
+    //berechnung des Durchschnit wertes jeder frage. Pro frage jeden antwort wert addieren und durch die anzahl gegebener antworten teilen.
+
+
+    // Farbbedingungen basierend auf dem Ergebnis wert der Antworten
+    if (0 <= $value && $value < 33) {
+        return 'progress-error';
+    } elseif (33 <= $value && $value < 66) {
+        return 'progress-warning';
+    } else {
+        return 'progress-success';
+    }
+}
--- resources/views/evaluateSurveyData.blade.php
+++ resources/views/evaluateSurveyData.blade.php
@@ -2,6 +2,7 @@
 
 @section('content')
     @php
+
         // Retrieve and decode the 'survey' parameter from the URL
            $survey = null;
         if (request()->query('survey'))
@@ -9,12 +10,22 @@
             // Retrieve and decode the 'survey' parameter from the URL
             $survey = json_decode(urldecode(request()->query('survey')), true); // Decode and convert to an array
         }
+        include_once resource_path('helpers/diffRoleAction.php');
+        $value = rand(1, 100);
+        $colour = calculateBewertungColour($value);
     @endphp
+    <!-- der value wert soll aus einer funktion gezogen werden, welche diesen aus den antworten der survey fragen berechnet.-->
     @if ($survey)
-        <x-card title=" {{ $survey['class'] }}">
-            <div class="flex flex-col space-y-4 md:flex-row md:items-center md:space-x-6 my-4">
-                <label for="ranking1" class="text-lg font-semibold text-gray-700">Frage 1:</label>
-                <x-rating id="ranking1" wire:model="ranking1" class="bg-warning" total="5" values="2" readonly />
+    <x-header title=" {{ $survey['class'] }}"></x-header>
+    <!--foreach Fragen ergebnis in wert-->
+        <x-card title="Frage: Nummer(ID)" class="shadow-md">
+            <div class="flex flex-cols-2 space-y-4 md:flex-row md:items-center md:space-x-6 my-4">
+            <div class="col-span-1 text-pretty">
+                <!--Die Frage.  event. {{-- $survey['frageInhalt'] --}}-->
+                Inhalt der Frage ############## ############ ####### ######## ########## ########### ################ ####### ################
+            </div> 
+            <x-progress id="ranking1" value="{{$value}}" max="100" class="h-6 {{$colour}}" label="{{$value}}"/>
+            <label for="ranking1" class="text-lg font-semibold text-gray-700">{{$value}}%</label>
             </div>
 
         </x-card>
