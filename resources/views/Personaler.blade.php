5c9486a925a6240b6ed9f70f2d175d596edd4cc1
DieBratkartoffel <juri.gontard@wieder.de>
2024-11-26 13:08:03 +0100

    Texte entfernt

--- resources/views/Personaler.blade.php
+++ resources/views/Personaler.blade.php
@@ -5,7 +5,7 @@
 <x-card shadow separator>
     some dids, dodads and thingimagigs
     <x-slot:figure>
-        <img src="{{ asset('Personaler_Banner.png') }}" /> {{--  QRCode--}}
+        <img src="{{ asset('Personaler_Banner.png') }}" />
     </x-slot:figure>
 </x-card>
 
